4351dd46941248f23606af823deba3ef6a4ca87f
Philippe GASSMANN <philoops@gmail.com>
2023-04-05 18:30:53 +0200

    Add configurable expiry on certificates

--- rlog-helper/src/main.rs
+++ rlog-helper/src/main.rs
@@ -2,11 +2,13 @@ use std::{
     error::Error,
     fs::{create_dir_all, File},
     io::{Read, Write},
+    path::Path,
 };
 
 use anyhow::Context;
 use clap::{Parser, Subcommand};
 use rcgen::{Certificate, CertificateParams, DistinguishedName, DnType, KeyPair};
+use time::OffsetDateTime;
 
 #[derive(Parser)]
 struct Opts {
@@ -42,17 +44,33 @@ enum CertificateCommand {
         organisation: Option<String>,
         #[arg(long)]
         organisation_unit: Option<String>,
+        /// When the certificate expires? in human time format (eg. "1M" = 1 month, "1y" = 1 year)
+        #[arg(long, default_value = "10y")]
+        expires_in: String,
         /// mandatory common name for this CA
         common_name: String,
     },
     /// Generate server certificate. output_dir must contain ca-priv-key.pem and ca.pem (output of generate-ca command)
     GenerateServer {
+        /// When the certificate expires? in human time format (eg. "1M" = 1 month, "1y" = 1 year)
+        #[arg(long, default_value = "1y")]
+        expires_in: String,
         #[arg(long)]
         alt_dns_hostname: Vec<String>,
         /// DNS hostname (will be put in the common name of the certificate)
         hostname: String,
     },
+    /// Generate client certificate.
+    ///
+    /// If the client certificate has already been generated (a private key for that client exists),
+    /// it will re-generate a new certificate using the same private key.
     GenerateClient {
+        /// When the certificate expires? in human time format (eg. "1M" = 1 month, "1y" = 1 year)
+        #[arg(long, default_value = "1y")]
+        expires_in: String,
+        /// Force the generation of a private key even if the key for the client exists.
+        #[arg(short, long)]
+        new_private_key: bool,
         /// Name of the client (common name)
         client_name: String,
     },
@@ -68,6 +86,7 @@ impl CertificateCommand {
                 organisation,
                 organisation_unit,
                 common_name,
+                expires_in,
             } => {
                 create_dir_all(&output_dir)
                     .with_context(|| format!("Unable to create output directory {output_dir}"))?;
@@ -78,6 +97,10 @@ impl CertificateCommand {
                     .distinguished_name
                     .push(DnType::CommonName, common_name);
                 params.alg = &rcgen::PKCS_ECDSA_P384_SHA384;
+                params.not_before = OffsetDateTime::now_utc();
+                params.not_after = params.not_before
+                    + humantime::parse_duration(&expires_in)
+                        .context("Unable to parse expires-in argument")?;
 
                 if let Some(country) = country {
                     params.distinguished_name.push(DnType::CountryName, country);
@@ -123,6 +146,7 @@ impl CertificateCommand {
                 }
             }
             CertificateCommand::GenerateServer {
+                expires_in,
                 alt_dns_hostname,
                 hostname,
             } => {
@@ -137,6 +161,10 @@ impl CertificateCommand {
                 params.distinguished_name = DistinguishedName::new();
                 params.distinguished_name.push(DnType::CommonName, hostname);
                 params.alg = &rcgen::PKCS_ECDSA_P384_SHA384;
+                params.not_before = OffsetDateTime::now_utc();
+                params.not_after = params.not_before
+                    + humantime::parse_duration(&expires_in)
+                        .context("Unable to parse expires-in argument")?;
 
                 let cert = Certificate::from_params(params)?;
                 {
@@ -158,7 +186,11 @@ impl CertificateCommand {
                     );
                 }
             }
-            CertificateCommand::GenerateClient { client_name } => {
+            CertificateCommand::GenerateClient {
+                expires_in,
+                client_name,
+                new_private_key,
+            } => {
                 let ca_certificate =
                     parse_ca_certificate(&output_dir).context("Unable to load CA certificates")?;
 
@@ -168,11 +200,27 @@ impl CertificateCommand {
                     .distinguished_name
                     .push(DnType::CommonName, client_name);
                 params.alg = &rcgen::PKCS_ECDSA_P384_SHA384;
+                params.not_before = OffsetDateTime::now_utc();
+                params.not_after = params.not_before
+                    + humantime::parse_duration(&expires_in)
+                        .context("Unable to parse expires-in argument")?;
+
+                let key_file_name = format!("{output_dir}/{client_name}.priv-key.pem");
+                let mut private_key_not_generated = false;
+                if !new_private_key {
+                    // ignore errors: if any error occurs reading private key PEM
+                    // (inexistent file, file corrupt), a new private key will be generated
+                    params.key_pair = load_keypair(&key_file_name).ok();
+                    private_key_not_generated = params.key_pair.is_some();
+                }
 
                 let cert = Certificate::from_params(params)?;
-                {
+                if private_key_not_generated {
+                    println!(
+                        "{client_name} existing client private key used for certificate generation"
+                    );
+                } else {
                     let key = cert.serialize_private_key_pem();
-                    let key_file_name = format!("{output_dir}/{client_name}.priv-key.pem");
                     File::create(&key_file_name)
                         .with_context(|| format!("Unable to open file {key_file_name}"))?
                         .write_all(key.as_bytes())?;
@@ -204,14 +252,23 @@ fn ca_cert_filename(output_dir: &str) -> String {
     format!("{output_dir}/ca.pem")
 }
 
+fn load_keypair<P: AsRef<Path>>(path: P) -> anyhow::Result<KeyPair> {
+    let path = path.as_ref();
+    Ok(KeyPair::from_pem(
+        &std::fs::read_to_string(path).with_context(|| {
+            format!("Unable to open private key from {}", path.to_string_lossy())
+        })?,
+    )
+    .context("Unable to parse private key PEM")?)
+}
+
 fn parse_ca_certificate(output_dir: &str) -> anyhow::Result<Certificate> {
     let ca_key_file_name = ca_key_filename(&output_dir);
     let mut ca_priv_pem = String::new();
     File::open(&ca_key_file_name)
         .with_context(|| format!("Unable to open CA private key from {ca_key_file_name}"))?
         .read_to_string(&mut ca_priv_pem)?;
-    let ca_keypair =
-        KeyPair::from_pem(&ca_priv_pem).context("Unable to parse CA private key PEM")?;
+    let ca_keypair = load_keypair(&ca_key_file_name).context("Unable to load CA private key")?;
 
     let ca_file_name = ca_cert_filename(&output_dir);
     let mut ca_pem = String::new();
